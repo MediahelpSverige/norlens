6bf9a421544890dd5209464a84a432745e337bb0
alienwizard <johanwendin@gmail.com>
2016-10-10 16:38:35 +0200

    change db

---
+++ wp-config.production.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Production environment config settings
+ *
+ * Enter any WordPress config settings that are specific to this environment 
+ * in this file.
+ * 
+ * @package    Studio 24 WordPress Multi-Environment Config
+ * @version    1.0
+ * @author     Studio 24 Ltd  <info@studio24.net>
+ */
+  
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'tmsab_se_tmsab');
+
+/** MySQL database username */
+define('DB_USER', 'tmsab_se_tmsab');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'steel1992');
+
+/** MySQL hostname */
+define('DB_HOST', 'tmsab.se.mysql');
+
+
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+
+define('WP_HOME', 'http://tmsab.se');
+define('WP_SITEURL', 'http://tmsab.se');
+
+
+$table_prefix  = 'wp_';
+
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+define('WP_DEBUG', false);
