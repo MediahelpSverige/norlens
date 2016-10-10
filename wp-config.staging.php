083ea5a33eb5b5a65d1391bc43aa7ca55a51393d
alienwizard <johanwendin@gmail.com>
2016-10-10 16:42:33 +0200

    change db

--- wp-config.staging.php
+++ wp-config.staging.php
@@ -37,6 +37,9 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+define('WP_HOME', 'http://norlens.mediahelpcrm.se/');
+define('WP_SITEURL', 'http://norlens.mediahelpcrm.se/');
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
